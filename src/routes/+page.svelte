d42eb29d2fdb3d94dc0b0836d4673ee8facc0730
Daniel Ho <hoduchai652007@gmail.com>
2023-11-28 12:49:28 +0700

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    let gif
+</script>
+
+<div class="home w-full h-screen text-center">
+    <h1 class="font-bold text-6xl mb-2">Magic Mirror</h1>
+    <p class="">A place where images can be seen through</p>
+    <br>
+    <div class="max-w-2xl left-0 right-0 m-auto"> 
+        <div class="flex gap-4 justify-center">
+            <a href="https://www.facebook.com/hoduchai07/" class="px-4 py-2 bg-blue-400 rounded-md">Contact For Work</a>
+            <a href="/image" class="px-4 py-2 bg-blue-400 rounded-md">Proceed On Magic Mirror</a>
+        </div>
+        <br>
+        <video autoplay muted loop>
+            <source bind:this={gif} src="/gifs/raiseEyeBrows.mp4" type="video/mp4">
+        </video>
+    </div>
+</div>
---
+++ src/routes/image/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+    let windowX = 0
+    let windowY = 0
+    let oldWindowX = 0
+    let oldWindowY = 0
+    let image
+    let imageWidth = 0
+    let imageHeight = 0
+    let maxWindowWidth = 0
+    let maxWindowHeight = 0
+    let translateX = 0
+    let translateY = 0
+
+    let isLoaded = false
+    // let meme = true
+	import { onMount } from "svelte";
+
+    onMount(() => {
+        // setTimeout(() => meme = false, 2000)
+        isLoaded = true
+        imageWidth = image.width
+        imageHeight = image.height
+        maxWindowWidth = window.outerWidth
+        maxWindowHeight = window.outerHeight
+        loop()
+    })
+    function keepImageCentered() {
+        if (isLoaded) {
+            windowX = window.screenX
+            windowY = window.screenY
+
+            translateX = maxWindowWidth/2 - windowX - imageWidth/2
+            translateY = maxWindowHeight/2 - windowY - imageHeight/2
+        }
+    }
+    function loop() {
+        setInterval(() => {
+            keepImageCentered()
+        }, 100)
+    }
+</script>
+
+
+<div class="w-full h-screen">
+    <img style={"transform: translate(" + translateX + "px," + translateY + "px)"} bind:this={image} class="max-w-5xl w-[64rem] absolute" src="/imgs/Spring.jpg" alt="">
+</div>
